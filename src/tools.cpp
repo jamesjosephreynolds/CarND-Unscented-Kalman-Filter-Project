ec256386003ee8c7c363ffe56bb09c49d70d135a
James Reynolds <jamesjosephreynolds@gmail.com>
2017-04-06 12:30:21 -0400

    Update tools.cpp
    Copy RMSE implementation from EKF repo

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,34 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  // Copy from EKF project
+  
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+    
+  // From Lesson 21
+  
+  if (estimations.size() != ground_truth.size()){
+    std::cout << "Array sizes must match!";
+    return rmse;
+  }
+  else if (estimations.size() == 0){
+    std::cout << "Arrays must be non-empty!";
+    return rmse;
+  }
+  
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd tmp = estimations[i]-ground_truth[i];
+    tmp = tmp.array()*tmp.array();
+    rmse += tmp;
+  }
+  
+  //calculate the mean
+  rmse = rmse/estimations.size();
+  
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
 }
